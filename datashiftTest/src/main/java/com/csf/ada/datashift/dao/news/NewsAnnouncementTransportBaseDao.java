bef71e2cede39efe5b9f7120783318359a509df3
xujunmeng <xujunmeng2012@163.com>
2019-02-13 12:36:32 +0800

    update

--- datashiftTest/src/main/java/com/csf/ada/datashift/dao/news/NewsAnnouncementTransportBaseDao.java
+++
@@ -1,7 +0,0 @@
-package com.csf.ada.datashift.dao.news;
-
-import com.csf.ada.datashift.dao.security.BaseDao;
-
-public class NewsAnnouncementTransportBaseDao extends BaseDao {
-
-}
--- datashiftTest/src/main/java/com/csf/ada/datashift/jmslistener/AbstractMessageListener.java
+++
@@ -1,8 +0,0 @@
-package com.csf.ada.datashift.jmslistener;
-
-import java.util.Date;
-import java.util.Map;
-
-public abstract class AbstractMessageListener {
-	protected abstract void executeTask(Date relDate , StringBuilder infoBuilder , Map<String , String> parm);
-}
