9fea0fb55f92c3fcc30068357f00504fab14904c
xujunmeng.github <xujunmeng2012@163.com>
2020-07-23 14:31:36 +0800

    Merge branch 'master' of https://github.com/xujunmeng/Test-Project-master
    # Conflicts:
    #	TestAlgorithm/.idea/workspace.xml
    #	datashiftTest/.idea/misc.xml
    #	datashiftTest/.idea/workspace.xml

--- TestSpringBean/src/test/java/第一个实例/BeanFactoryTest.java
+++ TestSpringBean/src/test/java/第一个实例/BeanFactoryTest.java
@@ -1,10 +1,10 @@
 package 第一个实例;
 
-import junit.framework.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
+import 自定义FactoryBean实现类.Car;
 
 /**
  * Created by james on 2017/8/28.
@@ -14,8 +14,8 @@ public class BeanFactoryTest {
     @Test
     public void testSimpleLoad() {
         BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("ApplicationContext.xml"));
-        MyTestBean bean = (MyTestBean)beanFactory.getBean("myTestBean");
-        Assert.assertEquals("testStr", bean.getTestStr());
+        Car bean = (Car)beanFactory.getBean("car");
+        System.out.println(bean);
     }
 
 }
