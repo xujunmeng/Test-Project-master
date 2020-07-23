43640f0dd2ee604d6d36ef7c1c90baea4abf8c3d
xujunmeng <xujunmeng2012@163.com>
2020-07-23 14:19:08 +0800

    Update ConsumerTest.java

--- TestOns/src/main/java/第一个实例/ConsumerTest.java
+++ TestOns/src/main/java/第一个实例/ConsumerTest.java
@@ -13,8 +13,8 @@ public class ConsumerTest {
 
         Properties properties = new Properties();
         properties.put(PropertyKeyConst.ConsumerId, "CID_junmeng_2");
-        properties.put(PropertyKeyConst.AccessKey, "LTAIUMThXTW47YlF");
-        properties.put(PropertyKeyConst.SecretKey, " 3qO7VN3buOyMpA9kB0pS7UexdU9mAO");
+        properties.put(PropertyKeyConst.AccessKey, "null");
+        properties.put(PropertyKeyConst.SecretKey, "null");
         Consumer consumer = ONSFactory.createConsumer(properties);
         //final String topic, final String subExpression, final MessageListener listener
         consumer.subscribe("test_topic_junmeng_2", "*", new MessageListener() {
